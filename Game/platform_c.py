5d751e4e4ab166768e5e5adfd0dd8896c0a78a62
himanshu <himanshusingh.chauhan2015@vit.ac.in>
2017-10-12 22:43:08 +0530

    Game Folder

---
+++ Game/platform_c.py
@@ -0,0 +1,20 @@
+import pygame
+pygame.init()
+
+class Platform(pygame.sprite.Sprite):
+      def __init__(self,screen):
+          pygame.sprite.Sprite.__init__(self)
+          self.screen = screen
+          self.image = pygame.image.load("platform2.png")
+          self.image = self.image.convert()
+          self.rect = self.image.get_rect()
+          self.rect.center = (395,624.5)
+          self.dx = -5
+
+      def update(self):
+          self.rect.centerx += self.dx 
+          self.checkbound()
+
+      def checkbound(self):
+          if self.rect.right <= self.screen.get_width():
+             self.rect.left = 0   
